04973252232834c1b8c9ab2200276c9e88cb7472
GuillaumePOREZ72 <guillaume.porez@oclock.school>
2025-09-29 16:13:07 +0200

    Implement question generation and solution checking functionality in App component

--- src/App.jsx
+++ src/App.jsx
@@ -49,9 +49,52 @@ function App() {
     setSolved(false);
     setCode(`function solution() {\n // Your code here\n}`);
     setQuestionData(null);
+
+    try {
+      const response = await window.puter.ai.chat(
+        `
+          Generate a random ${difficulty} level coding interview question like on LeetCode.
+          Return ONLY valid JSON with this structure:
+          {
+            "problem": "string",
+            "example": "string",
+            "constrations": "string",
+            "note": "string or empty if none"
+          }
+        `
+      );
+
+      const reply =
+        typeof response === "string"
+          ? response
+          : response.message?.content || "";
+
+      const parsed = JSON.parse(reply);
+
+      setQuestionData(parsed);
+    } catch (error) {
+      setFeedBack(`⛔ Error: ${error.message}`);
+    }
+    setLoading(false);
   };
 
-  const checkSolution = async () => {};
+  const checkSolution = async () => {
+    if (!code.trim()) return;
+
+    setLoading(true);
+    try {
+      const response = await window.puter.ai.chat(
+        `
+          You are a helpful interview coach.
+          The question is: "${questionData?.problem}".
+          Here is the candidate's
+        `
+      )
+    } catch (error) {
+      setFeedBack(`⛔ Error: ${error.message}`);
+    }
+    setLoading(false);
+  };
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-emerald-900 via-green-950 to-teal-900 flex flex-col items-center justify-center p-6 gap-10">
